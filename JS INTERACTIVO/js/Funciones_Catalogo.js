f598d6daca5528572473a43b02d760e56e6c992a
Rodrigo Aguilar <147280603+RAgCen@users.noreply.github.com>
2025-12-28 10:53:24 -0600

    Refactor product display and filter functionality

--- JS INTERACTIVO/js/Funciones_Catalogo.js
+++ JS INTERACTIVO/js/Funciones_Catalogo.js
@@ -1,5 +1,7 @@
 // Al hacer el import el archivo se convierte en un modulo
-import { productos } from "./Estructuras/Productos"; 
+import { productos } from "./Estructuras/Productos.js"; 
+
+import {getTotalArticulos, agregarProducto} from "./Funciones_Carrito.js";
 
 export function mostrarProducto(categoria){
     const contenedor = document.getElementById("productosContainer");
@@ -10,11 +12,11 @@ export function mostrarProducto(categoria){
 
     productosFiltrados.forEach(p => {
         const card = document.createElement("div");
-        card.classlist.add("col"); // Agregar la clase "col" para el diseño responsivo
+        card.classList.add("col"); // Agregar la clase "col" para el diseño responsivo
 
         card.innerHTML = `
             <div class="card border-2 shadow-sm p-4 h-100 text-center position-relative" data-aos="flip-up">
-                <button class="btn btn-sm position-absolute top-0 end-0 m-2 p-0 bg-transparent border-0 jinfo-btn">
+                <button class="btn btn-sm position-absolute top-0 end-0 m-2 p-0 bg-transparent border-0 info-btn">
                     <img src="/imagenes/acciones/info.png" width="28" height="28" class="rounded-circle shadow-sm" alt="Info">
                 </button>
 
@@ -26,15 +28,16 @@ export function mostrarProducto(categoria){
 
                 <div class="d-flex justify-content-center align-items-center gap-2">
                     <p class="mb-0">${p.estrellas} estrellas</p>
-                    <div>
-                        <img src="/imagenes/acciones/agregar.png" width="21" height="21" style="cursor:pointer;" alt="Agregar">
-                    </div>
                 </div>
             </div>`;
+
+            card.querySelector(".info-btn").addEventListener("click", () => {modal(p);})
+            contenedor.appendChild(card)
     });
 
-    card.querySelector(".info-btn").addEventListener("click", () => {modal(p);})
-    contenedor.appendChild(card);
+    if(typeof AOS !== 'undefined'){
+        AOS.refreshHard(); // Actualiza AOS para aplicar las animaciones a los nuevos elementos
+    }
 }
 
 
@@ -59,13 +62,13 @@ function modal(producto) {
         <img src="/imagenes/acciones/agregar.png" alt="Añadir al carrito" 
         width="22px" height="22px" class="me-5 btn-add-carrito" style="cursor: pointer;">`;
     
-    const imagenCarrito = divButton.querySelector('.btn-add-carrito');
+    const imagenAccionCarrito = divButton.querySelector('.btn-add-carrito');
 
     async function handlerAgregarProductos() {
         imagenAccionCarrito.removeEventListener('click', handlerAgregarProductos);
 
         divButton.innerHTML = 
-        `<label for="quantity g-5">Añadir al carrito:</label>
+        `<label for="quantity g-5">Procesando</label>
         <img src="/imagenes/gifs/procesando_2.gif" alt="Procesando" 
         width="22px" height="22px" class="me-5" style="cursor: wait;">`;
     
@@ -77,6 +80,8 @@ function modal(producto) {
         const cantidad = 1;
         const color = colorSelect.value;
 
+        agregarProducto(idProducto, cantidad, color)
+
         alert("Producto agregado al carrito correctamente.");
         divButton.innerHTML = 
         `<label for="quantity g-5">Producto agregado:</label>
@@ -84,9 +89,9 @@ function modal(producto) {
         width="22px" height="22px" class="me-5 btn-add-carrito" style="cursor: default;">`;
     }
 
-        imagenAccionCarrito.addEventListener('click', handlerAgregarProductos);
-        const modalProducto = new bootstrap.Modal(modal);
-        modalProducto.show();
+    imagenAccionCarrito.addEventListener('click', handlerAgregarProductos);
+    const modalProducto = new bootstrap.Modal(modal);
+    modalProducto.show();
 }
 
 
@@ -116,5 +121,151 @@ document.addEventListener("DOMContentLoaded", () => {
 
             }
         })
+    })
+
+    function aplicarFiltros(){
+        const categoriaActivaElement = document.querySelector('.navbar-categorias .nav-linl.active'); // Selecciona el enlace de categoría activo
+        const categoriaActiva = categoriaActivaElement ? categoriaActivaElement.dataset.categoria : "Electrónica"; // Si no hay categoría activa, se usa "Electrónica" por defecto
+
+        const filtroEstrellas = document.getElementById("filtroEstrellas").value; // Obtener el valor seleccionado del filtro de estrellas
+        const descuentoSeleccionado = Array.from(cbdescuento).find(cb => cb.checked); // Encuentra el checkbox seleccionado
+        const rangoPrecio = document.getElementById("rangoPrecio").value; // Obtener el valor del rango de precios
+
+        const estrellasMinimas = parseInt(filtroEstrellas, 10); // Convertir a número entero en base 10
+        const precioMax = parseInt(rangoPrecio, 10); // Convertir a número entero en base 10
+
+        // La siguiente validación asegura que al menos un filtro esté activo antes de aplicar los filtros
+        const filtrosActivos = estrellasMinimas>0 || descuentoSeleccionado || precioMax > 0;
+        if(!filtrosActivos){
+            alert("Seleccione al menos un filtro para aplicar.");
+            return;
+        }
+
+        let productosFiltrados = productos.filter(p => p.categoria.toLowerCase().trim() === categoriaActiva.toLowerCase().trim()); // Filtrar por categoría
+
+        // ---------- Construcción de filtros ----------
+
+        // Filtrar por estrellas
+        if(estrellasMinimas > 0){
+            productosFiltrados.filter(p => p.estrellas >= estrellasMinimas);
+        }
+
+        // Filtrar por descuento
+        if(descuentoSeleccionado){
+            let descMin = 0;
+            let descMax = 0;
+            const totalDesc = descuentoSeleccionado.id; // Obtener el id del checkbox seleccionado gracias a que se está trabajando con un array
+
+            // Dependiendo el id del checkbox seleccionado, se asignan los valores mínimos y máximos de descuento
+            if(totalDesc === "desc1"){
+                descMin = 5; descMax = 10;
+            }else if(totalDesc === "desc2"){
+                descMin = 10; descMax = 15;
+            }else if(totalDesc === "desc1"){
+                descMin = 30; descMax = 60;
+            }
+
+            productosFiltrados = productosFiltrados.filter(p => p.descuento >= descMin && p.descuento <= descMax);
+        }
+
+        // Filtro por precio
+        if(precioMax> 0){
+            productosFiltrados = productosFiltrados.filter(p => p.precio <= precioMax);
+        }
+
+        const contenedor = document.getElementById("productosContainer");
+        contenedor.innerHTML = ""; // Limpiar el contenedor antes de agregar nuevos productos
+
+
+
+        // ---------- Mostrar productos filtrados ----------
+        productosFiltrados.forEach(p => {
+            const card = document.createElement("div");
+            card.classList.add("col"); // Agregar la clase "col" para el diseño responsivo
+
+            card.innerHTML = `
+                <div class="card border-2 shadow-sm p-4 h-100 text-center position-relative" data-aos="flip-up">
+                    <button class="btn btn-sm position-absolute top-0 end-0 m-2 p-0 bg-transparent border-0 info-btn">
+                        <img src="/imagenes/acciones/info.png" width="28" height="28" class="rounded-circle shadow-sm" alt="Info">
+                    </button>
+
+                    <i class="fa-solid fa-box-open fa-3x bg-cartoon-box mb-3"></i>
+
+                    <h5 class="fw-bold">${p.nombre}</h5>
+                    <p class="text-muted mb-1">Modelo: ${p.modelo}</p>
+                    <p class="fw-semibold text-success">$${p.precio.toFixed(2)} MXN</p>
+
+                    <div class="d-flex justify-content-center align-items-center gap-2">
+                        <p class="mb-0">${p.estrellas} estrellas</p>
+                    </div>
+                </div>`;
+
+                card.querySelector(".info-btn").addEventListener("click", () => {modal(p);})
+                contenedor.appendChild(card)
+        });
+
+        if(typeof AOS !== 'undefined'){
+            AOS.refreshHard(); // Actualiza AOS para aplicar las animaciones a los nuevos elementos
+        }
+
+    }
+
+    function limpiarFiltros(){
+        document.getElementById("filtroEstrellas").value = "0"; // Reiniciar el filtro de estrellas
+
+        const cbdescuento = document.querySelectorAll('input[name^="desc"]'); // ^= indica que no se llama así el objeto, pero tiene la palabra "desc" en su nombre
+        cbdescuento.forEach(cb => {
+            cb.checked = false; // Desmarcar todos los checkboxes de descuento
+        });
+        const precioSpan = document.getElementById("valorPrecio");
+        if(precioSpan){
+            precioSpan.textContent = "$0 MXN"; // Reiniciar el texto del rango de precios
+        }
+
+        const categoriaActivaElement = document.querySelector('.navbar-categorias .nav-linl.active'); // Selecciona el enlace de categoría activo
+
+        mostrarProducto(categoriaActivaElement.trim()); // Mostrar productos de la categoría activa sin filtros
+    }
+
+    // ---------- Asignación de eventos a los botones ----------
+    const botonFiltro = document.getElementById("btn-filtrar");
+    if(botonFiltro)botonFiltro.addEventListener("click", aplicarFiltros);
+
+    const botonLimpiar = document.getElementById("btn-limpiar");
+    if(botonLimpiar)botonLimpiar.addEventListener("click", limpiarFiltros);
+
+
+    // ---------- Actualización del valor del rango de precios ---------
+    const rangoPrecio = document.getElementById("rangoPrecio");
+    const valorPrecio = document.getElementById("valorPrecio");
+
+    if(rangoPrecio && valorPrecio){
+        rangoPrecio.addEventListener("input", function(){ actualizarValorPrecio(this.value); });
+
+        function actualizarValorPrecio(valor){
+            const valorFinal = parseInt(valor, 10);
+
+            // Estructura correcta de moneda mexicana
+            const formatter = new Intl.NumberFormat('es-MX', {
+                style: 'currency',
+                currency: 'MXN',
+                maximumFractionDigits: 0, // Sin fracciones
+            });
+
+            if(valorFinal >= 10000){
+                valorPrecio.textContent = `${formatter.format(10000)}+ MXN`;
+            }else{
+                valorPrecio.textContent = `${formatter.format(valorFinal)} MXN`;
+            };
+        }
+    }
+
+
+    // Actualizar el badge del carrito en el inicio.html
+    document.addEventListener("DOMContentLoaded", () => {
+        const spanCarrito = document.getElementById("spanCarrito");
+        if(spanCarrito){
+            spanCarrito.textContent = getTotalArticulos();
+        }
     })
 })
